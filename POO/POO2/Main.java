70f6512bcb46d14c54231eb5fe4724d183f2939b
roma.prd <deyvyalex2006@gmail.com>
2024-10-22 17:51:53 -0300

    Main.java

--- POO/POO2/Main.java
+++ POO/POO2/Main.java
@@ -5,16 +5,6 @@ public class Main {
         Pessoa pessoa1 = new Pessoa();
         Pessoa pessoa2 = new Pessoa();
 
-        //pessoa1.nome = "romaprd";
-        //pessoa1.idade = 18;
-
-        //pessoa2.nome = "romario";
-        //pessoa2.idade = 19;
-
-        //pessoa2.informacoes();
-        //System.out.println("\n");
-        //pessoa1.informacoes();
-
         pessoa1.setNome("romaprd");
         pessoa1.setIdade(18);
 
